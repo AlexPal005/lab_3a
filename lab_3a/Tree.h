4e08a62c6f5bd82e39163954523c13bed1580005
AlexPal005 <rudenkoolexandr06@knu.ua>
2022-06-18 17:17:57 +0300

    add insert and delete

--- lab_3a/Tree.h
+++ lab_3a/Tree.h
@@ -1,5 +1,31 @@
 #pragma once
-class Tree
-{
+#include <iostream>
+using namespace std;
+/**
+*  Структура вузла, що містить ключ, вказівники на батька та потомків, колір вузла
+*/
+struct node {
+	int key;
+	node* left;
+	node* right;
+	node* parent;
+	char color;
 };
+class Tree {
+public:
+	Tree() {
+		q = NULL;
+		root = NULL;
+	}
+	void insert();/**Вставка елемента*/
+	void insertfix(node*);
+	void leftrotate(node*);/**Правий поворот*/
+	void rightrotate(node*);/**Лівий поворот*/
+	void disp();/**Показати дерево*/
+	void display(node*);
+private:
+	node* root; /** Вказівник на корінь*/
+	node* q;
 
+
+};
--- lab_3a/lab_3a.cpp
+++ lab_3a/lab_3a.cpp
@@ -1,20 +1,34 @@
-﻿// lab_3a.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
+﻿#include "Tree.h"
+int main() {
+    int ch, y = 0;
+    Tree obj;
+    do
+    {
+        cout << "\n\t RED BLACK TREE ";
+        cout << "\n 1. Insert in the tree ";
+        cout << "\n 2. Delete a node from the tree";
+        cout << "\n 3. Search for an element in the tree";
+        cout << "\n 4. Display the tree ";
+        cout << "\n 5. Exit ";
+        cout << "\nEnter Your Choice: ";
+        cin >> ch;
+        switch (ch)
+        {
+        case 1: obj.insert();
+            cout << "\nNode Inserted.\n";
+            break;
+        case 2: //obj.del();
+            break;
+        case 3: //obj.search();
+            break;
+        case 4: obj.disp();
+            break;
+        case 5: y = 1;
+            break;
+        default: cout << "\nEnter a Valid Choice.";
+        }
+        cout << endl;
 
-#include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+    } while (y != 1);
+    return 1;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
